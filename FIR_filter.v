56d9fa8fc6dd69c5f8fbb5686a583596e6c17d80
Fabio Luna <fabioluna20@gmail.com>
2026-03-25 14:29:56 +0100

    update fir filter

--- FIR_filter.v
+++ FIR_filter.v
@@ -1,5 +1,22 @@
  module FIR_filter
- (
- 
- 
+ #(
+	parameter b0 = 2,
+	parameter b1 = 3,
+	
+	parameter NB_IN = 10,
+	parameter NB_OUT = 25
  )
+ (
+	input clk,
+	input      signed [NB_IN - 1:0] x, 
+	output reg signed [NB_OUT- 1:0] y
+ );
+ 
+always @ (posedge clk)
+begin
+	y <= x;
+	
+
+end
+ 
+endmodule 
