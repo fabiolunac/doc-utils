309a3fcd954e26061692ff488f5eff0e533463da
Fabio Luna <fabioluna20@gmail.com>
2026-03-25 15:51:17 +0100

    generic fir filter completed

--- FIR_filter.v
+++ FIR_filter.v
@@ -2,7 +2,8 @@
  #(
 	parameter NB_IN = 10,
 	parameter NB_OUT = 25,
-	parameter NB_W = 10
+	parameter NB_W = 10,
+	parameter NUMBER_W = 2
  )
  (
 	input clk,
@@ -12,20 +13,43 @@
  );
  
 // READ WEIGHTS
-reg [NB_W - 1:0] b [0:1];
+reg [NB_W - 1:0] b [0:NUMBER_W-1];
 initial begin
     $readmemb("weights.mem", b); // read in binary
 end
 
-reg signed [NB_IN - 1:0] rx;
- 
+// shifter registers 
+reg signed [NB_IN - 1:0] sr [NUMBER_W - 2:0];
+
+integer i;
+// accumulator
+reg signed [NB_OUT-1:0] acc;
+
 always @ (posedge clk) begin
 	if(rst) begin
-		rx <= 0;
+		sr[0] <= 0;
 		y <= 0;
 	end else begin
-		rx <= x;
-		y <= b[0]*x + b[1]*rx;
+		sr[0] <= x;
+		y <= b[0]*x + b[1]*sr[0];
 	end
 end
+
+always @ (posedge clk) begin
+    if  (rst) begin
+        y <= 0;
+        for (i = 0; i < NUMBER_W-1; i = i + 1)
+            sr[i] <= 0;
+    end else begin
+        sr[0] <= x;
+        for (i = 1; i < NUMBER_W-1; i = i + 1)
+            sr[i] <= sr[i-1];
+        
+        acc = b[0] * x;
+        for (i = 1; i < NUMBER_W; i = i + 1)
+            acc = acc + b[i]*sr[i-1];   
+        y <= acc;
+    end 
+end
+
 endmodule 
