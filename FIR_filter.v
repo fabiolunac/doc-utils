54df9fff1bdc6db4ba1d35f087bca75991fdadd7
Fabio Luna <fabioluna20@gmail.com>
2026-03-25 15:32:47 +0100

    update fir filter

--- FIR_filter.v
+++ FIR_filter.v
@@ -1,22 +1,31 @@
  module FIR_filter
  #(
-	parameter b0 = 2,
-	parameter b1 = 3,
-	
 	parameter NB_IN = 10,
-	parameter NB_OUT = 25
+	parameter NB_OUT = 25,
+	parameter NB_W = 10
  )
  (
 	input clk,
-	input      signed [NB_IN - 1:0] x, 
+	input rst,
+	input  signed [NB_IN - 1:0] x, 
 	output reg signed [NB_OUT- 1:0] y
  );
  
-always @ (posedge clk)
-begin
-	y <= x;
-	
-
+// READ WEIGHTS
+reg [NB_W - 1:0] b [0:1];
+initial begin
+    $readmemb("weights.mem", b); // read in binary
 end
+
+reg signed [NB_IN - 1:0] rx;
  
+always @ (posedge clk) begin
+	if(rst) begin
+		rx <= 0;
+		y <= 0;
+	end else begin
+		rx <= x;
+		y <= b[0]*x + b[1]*rx;
+	end
+end
 endmodule 
