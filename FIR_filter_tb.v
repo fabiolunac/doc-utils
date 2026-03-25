dcb89f7ec8b4795ccd10e2271695ab69160291cb
Fabio Luna <fabioluna20@gmail.com>
2026-03-25 15:30:13 +0100

    testbench file

---
+++ FIR_filter_tb.v
@@ -0,0 +1,25 @@
+module FIR_filter_tb();
+
+reg clk;
+reg rst;
+reg signed [9:0] x;
+wire signed [24:0] y;
+
+initial begin
+	clk = 0;
+	rst = 1;
+	x = 0;
+	
+	#12 rst = 0;
+	
+	#3 x = 1;
+	#10 x = 0;
+	
+	#30 $finish;
+end
+
+always #5 clk = ~clk;
+
+FIR_filter fir(clk, rst, x, y);
+
+endmodule
